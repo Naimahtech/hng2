1ce3f71ce5233ac95924ff303153702c15ef1f8f
Naimahtech <naimatoyewale@yahoo.com>
2018-04-14 17:04:07 +0100

    adding files

---
+++ hng/hng2.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>figma design implementation</title>
+	<style type="text/css">
+	html{
+		background-color: #00000f;
+       	}
+		body{
+			background:url("background9.png");
+			background-blend-mode: screen;
+		    background-repeat: no-repeat;
+	        background-position: center center;
+			color: green;
+			font-size: 20px;
+			opacity:;
+		}
+		div{
+			color: maroon;
+			font-family: arial, sans-serif;
+			font-size: 20px;
+			margin-top: 20px;
+			height: 604px;
+			width:880px;
+		}
+		h2{
+			text-align: center;
+			margin-top: 20px;
+			opacity: ;
+
+		}
+		
+	</style>
+	<link href="https://fonts.googleapis.com/css?family=Signika:400" rel="stylesheet">
+</head>
+<body>
+	<div>
+<h2><?php 
+            $time = date("h:i:sa");
+            echo $time 
+            ?></h2>
+			</div>
+
+</body>
+</html>
